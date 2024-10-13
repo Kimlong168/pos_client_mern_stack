e62f8ab4e501a1b840faf8b787d8fd839e4f5369
kimlong <kimlong5244@gmail.com>
2024-10-13 23:54:40 +0700

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,25 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   resolve: {
-//     alias: {
-//       "@": "/src", // Use the root-based alias directly
-//     },
-//   },
-// });
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import netlify from "vite-plugin-netlify"; // Import Netlify plugin
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), netlify()],
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": "/src",
+      "@": "/src", // Use the root-based alias directly
     },
   },
 });
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import netlify from "vite-plugin-netlify"; // Import Netlify plugin
+
+// export default defineConfig({
+//   plugins: [react(), netlify()],
+//   resolve: {
+//     alias: {
+//       "@": "/src",
+//     },
+//   },
+// });
